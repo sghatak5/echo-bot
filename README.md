f369ecf1a8b91c457f7c708afc4ffb2d8f7d0f7b
sghatak5 <sagnikghatak22@gmail.com>
2024-03-05 17:24:13 +0100

    Drive Robot in gazebo

--- README.md
+++ README.md
@@ -22,7 +22,16 @@ This GitHub project focuses on building a bot using ROS2 and simulate it in Gaze
 ```sh
 $ sudo apt install ros-<distro-name>-xacro ros-<distro-name>-joint-state-publisher-gui
 ```
+- Install Gazebo and integrate it with ROS
+```sh
+$ sudo apt install ros-<distro-name>-gazebo-ros-pkgs
+```
+
 # Robot 3D Model
 Create a differential drive robot model using multiple configuration files in the URDF (Unified Robot Description Format) and processing them together using Xacro, a macro language for URDF. The final URDF file generated is then passed into Robot State Publisher, facilitating the availability of the URDF data in the robot_description topic. To manipulate the robot's joints, Robot State Publisher expects input from the joint_state topic. In the initial phase of the project, the Joint State Publisher GUI is utilized to provide simulated joint values to the joint_state topic.
 ![Screenshot from 2024-03-05 16-36-56](https://github.com/sghatak5/echo-bot/assets/149153121/f445f8d7-c301-4615-a743-13796c73a220)
+
+# Simulating Robot in Gazebo
+ To simulate the robot in the Gazebo environment using ROS (Robot Operating System) a simulation is initiated using the launch_sim.launch.py script, which orchestrates the launch of essential components including Robot State Publisher, Gazebo simulator, and the robot model spawned within Gazebo. To enable locomotion control, a Gazebo control plugin is utilized to publish values to the left and rigth wheel transform, while user input from the keyboard is translated into velocity commands published to the cmd_vel topic.
+[screencast-from-03-05-2024-051907-pm_3n1loKbU.webm](https://github.com/sghatak5/echo-bot/assets/149153121/a23a7508-c1b2-416e-b1c2-0c15bc8fdeba)
 
---
+++ echo_bot_pkg/launch/launch_sim.launch.py
@@ -0,0 +1,75 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+
+def generate_launch_description():
+
+    package_name='echo_bot_pkg'
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    # joystick = IncludeLaunchDescription(
+    #             PythonLaunchDescriptionSource([os.path.join(
+    #                 get_package_share_directory(package_name),'launch','joystick.launch.py'
+    #             )]), launch_arguments={'use_sim_time': 'true'}.items()
+    # )
+
+    # twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    # twist_mux = Node(
+    #         package="twist_mux",
+    #         executable="twist_mux",
+    #         parameters=[twist_mux_params, {'use_sim_time': True}],
+    #         remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+    #     )
+
+    #gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    #launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+             )
+
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_bot'],
+                        output='screen')
+
+
+    # diff_drive_spawner = Node(
+    #     package="controller_manager",
+    #     executable="spawner",
+    #     arguments=["diff_cont"],
+    # )
+
+    # joint_broad_spawner = Node(
+    #     package="controller_manager",
+    #     executable="spawner",
+    #     arguments=["joint_broad"],
+    # )
+
+    # Launch them all!
+    return LaunchDescription([
+        rsp,
+        # joystick,
+        # twist_mux,
+        gazebo,
+        spawn_entity,
+        # diff_drive_spawner,
+        # joint_broad_spawner
+    ])
